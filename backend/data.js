c8f2ec931281f0a6c301aabf87d66da58e130375
Luqman Shofuleji <luqmanshof@gmail.com>
2020-08-25 23:26:45 +0100

    first commit

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.6,
+            numReviews: 8,
+            countInStock: 5
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 3,
+            countInStock: 6
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 2.5,
+            numReviews: 6,
+            countInStock: 7
+        },
+        {
+            _id: '4',
+            name: 'Short Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 2,
+            countInStock: 0
+        }
+    ]
+}
---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 3
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 2.5,
+            numReviews: 6
+        },
+        {
+            _id: '4',
+            name: 'Short Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 2
+        }
+    ]
+}
