0e59a4fe0c8bd8673974a5def49c2d019079493b
Luqman Shofuleji <luqmanshof@gmail.com>
2020-08-30 11:04:43 +0100

    publish

--- backend/config.js
+++ backend/config.js
@@ -5,5 +5,6 @@ dotenv.config();
 export default {
   MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/amazona",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
-  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  PORT: process.env.PORT || 5000
 };
