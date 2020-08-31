643865de41aa1b53b362a2d77b9942e981eb258b
Luqman Shofuleji <luqmanshof@gmail.com>
2020-08-31 05:29:32 +0100

    heroku

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 })
 
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
