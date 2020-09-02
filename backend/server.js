2ba4dfb15074ec71042168641d8464f68b2ac264
Luqman Shofuleji <luqmanshof@gmail.com>
2020-09-02 02:34:30 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(config.PORT, () => {
   console.log("Server started at http://localhost:5000");
 });
